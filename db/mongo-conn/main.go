49836704db79529417525747a41299ce5f0f9ec5
Artem Lialin <zZ87111ff@gmail.com>
2023-10-30 23:06:37 +0300

    switched to mongo-driver

--- db/mongo-conn/main.go
+++ db/mongo-conn/main.go
@@ -2,33 +2,50 @@ package main
 
 import (
 	// inner
+	"context"
 	"fmt"
 	"log"
+	"time"
 
 	// outer
-	mgo "gopkg.in/mgo.v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
 type Transaction struct {
-	CCnum      string `bson:"ccnum"`
-	Date       string `bson:"date"`
-	Amount     string `bson:"amount"`
-	Cvv        string `bson:"cvv"`
-	Expiration string `bson:"exp"`
+	CCnum      string  `bson:"ccnum"`
+	Date       string  `bson:"date"`
+	Amount     float64 `bson:"amount"`
+	Cvv        string  `bson:"cvv"`
+	Expiration string  `bson:"exp"`
 }
 
-func main(){
-	session,err:=mgo.Dial("127.0.0.1")
-	if err!=nil{
-		log.Panicln(err)
-	}
-	defer session.Close()
+var client *mongo.Client
+var collection *mongo.Collection
 
-	res:= make([]Transaction,0)
-
-	if err:=session.DB("store").C("transactions").Find(nil).All(&res); err!=nil{
-		log.Panicln(err)
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+	DB := client.Database("store")
+	collection = DB.Collection("transactions")
+	cursor, err := collection.Find(context.TODO(), bson.D{})
+	if err != nil {
+		log.Fatal(err)
 	}
-	for _,txn := range res{
-		fmt.Println(txn.CCnum,txn.Date,txn.Amount,txn.Cvv,txn.Expiration)
+
+	res := make([]Transaction, 0)
+	if err = cursor.All(context.TODO(), &res); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, txn := range res {
+		fmt.Println(txn.CCnum, txn.Date, txn.Amount, txn.Cvv, txn.Expiration)
 	}
 }
